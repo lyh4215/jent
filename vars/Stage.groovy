23c5025779258e078b9850ea4454ba51a5735346
Wonho Bae <77373106+lyh4215@users.noreply.github.com>
2026-02-12 22:42:18 +0000

    feat: Add 'when' option to Stage step for conditional execution

--- vars/Stage.groovy
+++ vars/Stage.groovy
@@ -1,9 +1,24 @@
 import org.company.when.SkipStageException
 import org.company.failure.FailureRegistry
 
-def call(String id, Closure body) {
+def call(String id, Map opts = [:], Closure body) {
 
     stage(id) {
+
+        // --- When 처리 ---
+        if (opts?.when) {
+            def policy = resolvePolicy(opts.when)
+
+            if (!policy.allows(this)) {
+
+                catchError(buildResult: 'SUCCESS', stageResult: 'NOT_BUILT') {
+                    error("Skipped by When policy: ${policy.class.simpleName}")
+                }
+
+                return
+            }
+        }
+        
         try {
             body.call()
         } catch (SkipStageException se) {
