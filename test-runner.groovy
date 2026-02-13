5e923743f6aeb27f7ec8510f8f808437c89c2267
Wonho Bae <77373106+lyh4215@users.noreply.github.com>
2026-02-13 22:34:56 +0000

    chore: delete unused code

--- test-runner.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env groovy
-this.class.classLoader.addClasspath("src")
-this.class.classLoader.addClasspath("test")
-
-import org.company.ci.FailureClassifierTest
-
-FailureClassifierTest.run()
-//groovy -cp src:test test-runner.groovy
