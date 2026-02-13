52d107205efffd41f91b251b96c1bc63810f139c
Wonho Bae <77373106+lyh4215@users.noreply.github.com>
2026-02-13 07:32:53 +0000

    feat: write currentBuild description

--- src/org/company/failure/FailureLogAction.groovy
+++ src/org/company/failure/FailureLogAction.groovy
@@ -5,5 +5,6 @@ class FailureLogAction implements FailureAction {
     @Override
     void execute(def script, FailureContext ctx) {
         script.echo "[LOG] failedAt=${ctx.stageId}"
+        script.currentBuild.description = "Failed at stage: ${ctx.stageId}"
     }
 }
